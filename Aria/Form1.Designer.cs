2d6b00e8a66c4211c6b714d36b256dea76bbfabe
Justin Gewecke <Icefireburn1@yahoo.com>
2017-08-26 17:14:21 -0500

    Adding URL box

--- Aria/Form1.Designer.cs
+++ Aria/Form1.Designer.cs
@@ -28,12 +28,44 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dwnld_Button = new System.Windows.Forms.Button();
+            this.urlBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // dwnld_Button
+            // 
+            this.dwnld_Button.Location = new System.Drawing.Point(103, 226);
+            this.dwnld_Button.Name = "dwnld_Button";
+            this.dwnld_Button.Size = new System.Drawing.Size(75, 23);
+            this.dwnld_Button.TabIndex = 0;
+            this.dwnld_Button.Text = "button1";
+            this.dwnld_Button.UseVisualStyleBackColor = true;
+            // 
+            // urlBox
+            // 
+            this.urlBox.Location = new System.Drawing.Point(91, 171);
+            this.urlBox.Name = "urlBox";
+            this.urlBox.Size = new System.Drawing.Size(100, 20);
+            this.urlBox.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.urlBox);
+            this.Controls.Add(this.dwnld_Button);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button dwnld_Button;
+        private System.Windows.Forms.TextBox urlBox;
     }
 }
 
