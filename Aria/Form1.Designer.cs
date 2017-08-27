20977d00e31a3877e9650c372ae7b861d7e76ed0
Justin Gewecke <Icefireburn1@yahoo.com>
2017-08-26 21:43:09 -0500

    Added ability to download Youtube videos.

--- Aria/Form1.Designer.cs
+++ Aria/Form1.Designer.cs
@@ -30,29 +30,59 @@
         {
             this.dwnld_Button = new System.Windows.Forms.Button();
             this.urlBox = new System.Windows.Forms.TextBox();
+            this.convertMp3 = new System.Windows.Forms.CheckBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
             this.SuspendLayout();
             // 
             // dwnld_Button
             // 
-            this.dwnld_Button.Location = new System.Drawing.Point(103, 226);
+            this.dwnld_Button.Location = new System.Drawing.Point(101, 171);
             this.dwnld_Button.Name = "dwnld_Button";
             this.dwnld_Button.Size = new System.Drawing.Size(75, 23);
             this.dwnld_Button.TabIndex = 0;
-            this.dwnld_Button.Text = "button1";
+            this.dwnld_Button.Text = "Download";
             this.dwnld_Button.UseVisualStyleBackColor = true;
+            this.dwnld_Button.Click += new System.EventHandler(this.dwnld_Button_Click);
             // 
             // urlBox
             // 
-            this.urlBox.Location = new System.Drawing.Point(91, 171);
+            this.urlBox.Location = new System.Drawing.Point(56, 200);
             this.urlBox.Name = "urlBox";
-            this.urlBox.Size = new System.Drawing.Size(100, 20);
+            this.urlBox.Size = new System.Drawing.Size(169, 20);
             this.urlBox.TabIndex = 1;
+            this.urlBox.WordWrap = false;
+            this.urlBox.Click += new System.EventHandler(this.urlBox_Click);
+            // 
+            // convertMp3
+            // 
+            this.convertMp3.AutoSize = true;
+            this.convertMp3.Location = new System.Drawing.Point(176, 177);
+            this.convertMp3.Name = "convertMp3";
+            this.convertMp3.Size = new System.Drawing.Size(49, 17);
+            this.convertMp3.TabIndex = 2;
+            this.convertMp3.Text = ",mp3";
+            this.convertMp3.UseVisualStyleBackColor = true;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(0, 238);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(284, 23);
+            this.progressBar1.TabIndex = 3;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.convertMp3);
             this.Controls.Add(this.urlBox);
             this.Controls.Add(this.dwnld_Button);
             this.Name = "Form1";
@@ -66,6 +96,9 @@
 
         private System.Windows.Forms.Button dwnld_Button;
         private System.Windows.Forms.TextBox urlBox;
+        private System.Windows.Forms.CheckBox convertMp3;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
     }
 }
 
--- Aria/Form1.cs
+++ Aria/Form1.cs
@@ -7,14 +7,75 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
+using VideoLibrary;
+using System.IO;
 
 namespace Aria
 {
     public partial class Form1 : Form
     {
+        public float progress;
+
         public Form1()
         {
             InitializeComponent();
+            progressBar1.Maximum = 100;
+            progressBar1.Step = 1;
+            progressBar1.Value = 0;
+        }
+
+        private void urlBox_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dwnld_Button_Click(object sender, EventArgs e)
+        {
+            if (convertMp3.Checked)
+            {
+
+            }
+            else
+            {
+                backgroundWorker1.RunWorkerAsync();
+            }
+        }
+
+        private void findNum()
+        {
+
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            BackgroundWorker backgroundWorker = (BackgroundWorker)sender;
+            backgroundWorker.WorkerReportsProgress = true;
+
+            YouTube youtube = new YouTube();
+            Video video = youtube.GetVideo(urlBox.Text);
+
+            string title = video.Title;
+            Byte[] videoBytes = video.GetBytes();
+
+            FileStream fs = new FileStream(@"C:\Users\Icefireburn1\Videos\" + video.FullName, FileMode.Create,FileAccess.Write);
+            for (int offset = 0; offset < videoBytes.Length; offset += 65536)
+            {
+                fs.Write(videoBytes, offset, Math.Min(65536, videoBytes.Length - offset));
+                float percent = ((float)offset / (float)videoBytes.Length) * 100f;
+                
+                backgroundWorker.ReportProgress((int)Math.Round(percent, MidpointRounding.AwayFromZero));
+
+                Thread.Sleep(1);
+            }
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            if (e.ProgressPercentage >= 0 && e.ProgressPercentage <= 100)
+            {
+                progressBar1.Value = e.ProgressPercentage;
+            }
         }
     }
 }
